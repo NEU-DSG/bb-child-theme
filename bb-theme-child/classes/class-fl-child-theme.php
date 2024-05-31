7980a0f12f8619a37d0fefa241819c5047a390e7
JeanineNR <je.rodriguez@northeastern.edu>
2024-05-31 10:28:05 -0400

    Update to theme css and adding miscellaneous js file

--- bb-theme-child/classes/class-fl-child-theme.php
+++ bb-theme-child/classes/class-fl-child-theme.php
@@ -6,7 +6,7 @@
  * @class FLChildTheme
  */
 final class FLChildTheme {
-    
+
     /**
 	 * Enqueues scripts and styles.
 	 *
@@ -15,5 +15,11 @@ final class FLChildTheme {
     static public function enqueue_scripts()
     {
 	    wp_enqueue_style( 'fl-child-theme', FL_CHILD_THEME_URL . '/style.css' );
+	    wp_enqueue_style( 'fl-child-theme', FL_CHILD_THEME_URL . '/plugins-all.min.css' );
+	    wp_enqueue_script( 'fl-child-theme', FL_CHILD_THEME_URL . '/gallery.js' );
+	    wp_enqueue_script( 'fl-child-theme', FL_CHILD_THEME_URL . '/imagesloaded.min.js' );
+	    wp_enqueue_script( 'fl-child-theme', FL_CHILD_THEME_URL . '/masonry.min.js' );
+	    wp_enqueue_script( 'fl-child-theme', FL_CHILD_THEME_URL . '/quest-and-plugins.js' );
+	    wp_enqueue_script( 'fl-child-theme', FL_CHILD_THEME_URL . '/freewall.js' );   
     }
 }
---
+++ readme.md
@@ -0,0 +1,7 @@
+# BB-Child-Theme
+A custom child theme of Beaver Builder for the CERES project.
+
+Designed by Jeanine Rodriguez
+
+# 1.1 update
+- Edited the inherit font size css class along with some fixed color choices so it can be managed within Beaver Builder's "customize" theme settings.
